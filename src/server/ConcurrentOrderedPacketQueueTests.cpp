c70a3d52cf9567707663c36515afc10ea978d210
Jamie McDonald <Jamie.McDonald@paconsulting.com>
2021-09-17 13:46:47 +0000

    ODED-178 Add tests to check that no packet is return if next packet in sequence is not top of q

--- src/server/ConcurrentOrderedPacketQueueTests.cpp
+++ src/server/ConcurrentOrderedPacketQueueTests.cpp
@@ -29,6 +29,18 @@ TEST_CASE("ConcurrentOrderedPacketQueue.")
     REQUIRE(std::string(packet.payload.begin(), packet.payload.end()) == "AB");
   }
 
+  SECTION("ConcurrentOrderedPacketQueue returns the 'waiting' status "
+          "and no packet if the packet at the top of the queue is not the next in the sequence")
+  {
+    queue.emplace({HeaderParams{0, 2, false, {}}, {'A', 'B'}});
+    queue.emplace({HeaderParams{0, 3, false, {}}, {'C', 'D'}});
+    queue.emplace({HeaderParams{0, 4, false, {}}, {'E', 'F'}});
+    auto queueResponse = queue.nextInSequencePacket(1, 0);
+
+    REQUIRE(queueResponse.first == ConcurrentOrderedPacketQueue::sequencedPacketStatus::waiting);
+    REQUIRE_FALSE(queueResponse.second.has_value());
+  }
+
   SECTION("ConcurrentOrderedPacketQueue discards duplicated packets and returns the 'discared' status")
   {
     queue.emplace({HeaderParams{0, 5, false, {}}, {'A', 'B'}});
